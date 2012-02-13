928d98e3e158e875c9c321f2d014c52a207bf44e
bolo.sall89@gmail.com <bolo.sall89@gmail.com@ba0d3a1c-73fe-7854-969d-052fc593b580>
2012-02-13 10:13:59 +0000

    - Struts 2 fonctionnel (manquait une librairie) - Si vous voulez tester un exemple de phase de jeu lancer testJQuery.jsp - Creation des packages et dossier mais sera a modifié... - Creation des actions playCrossword pour lorsque on a selectionné une grille du genre jouer_mots_croisé?id=3, la grille avec l'id 3 est choisie... mais pas de contenu.

---
+++ trunk/Cruciverbiste/src/actions/PlayArrowordAction.java
@@ -0,0 +1,29 @@
+package actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@SuppressWarnings("serial")
+public class PlayArrowordAction extends ActionSupport {
+	private int idGrid;
+	//private Grid grid
+	
+	public PlayArrowordAction() {
+		super();
+		idGrid = -1;
+	}
+	
+	public String execute() throws Exception {
+		//selectionner la grille avec l'id idGrid
+		//si elle existe remplir le champ grid avec
+		return SUCCESS;
+		//sinon retourner return ERROR;
+	}
+	
+	public int getIdGrid() {
+		return idGrid;
+	}
+	
+	public void setIdGrid(int idGrid) {
+		this.idGrid = idGrid;
+	}
+}
