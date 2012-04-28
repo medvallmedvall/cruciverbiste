5b3f51dc476252b32c99ed7b23f3ae03e89d90f4
cyrille.nantchouang <cyrille.nantchouang@ba0d3a1c-73fe-7854-969d-052fc593b580>
2012-04-28 13:30:37 +0000

    re

--- Cruciverbiste/src/actions/PlayAction.java
+++ Cruciverbiste/src/actions/PlayAction.java
@@ -2,16 +2,25 @@ package actions;
 
 
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import javax.xml.ws.http.HTTPBinding;
 
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.DefaultTextProvider;
 
 import dao.GrilleDao;
+import dao.GrilleEnCoursUtilisateurDao;
+import dao.LettreEnCoursDao;
 
 import entities.Grille;
+import entities.GrilleEnCoursUtilisateur;
+import entities.LettreEnCour;
 
 @SuppressWarnings("serial")
-public class PlayAction extends ActionSupport {
+public class PlayAction extends ActionSupport{
 	private int idGrille;
 	//private Grilles grille;
 	private Grille grille;
@@ -25,8 +34,42 @@ public class PlayAction extends ActionSupport {
 	public String execute() {
 		//selectionner la grille avec l'id idGrid
 		GrilleDao dao = new GrilleDao();
+		String listLettreEnCour=null;
 		try {
 			grille = dao.findById(idGrille);
+			Map<String, Object> session = ActionContext.getContext().getSession();
+			
+			if(session.containsKey("idUser")){
+				GrilleEnCoursUtilisateurDao grilleEnCoursUtilisateurDao=new GrilleEnCoursUtilisateurDao();
+				GrilleEnCoursUtilisateur grilleEnCoursUtilisateur=new GrilleEnCoursUtilisateur();
+	
+				grilleEnCoursUtilisateur=grilleEnCoursUtilisateurDao.findByGrilleUser(grille.getIdGrille(), (Integer) session.get("idUser"));
+				
+				LettreEnCoursDao lettreEnCoursDao=new LettreEnCoursDao();
+	
+				if(grilleEnCoursUtilisateur!=null){
+					listLettreEnCour=lettreEnCoursDao.findByid(grille.getIdGrille(), grilleEnCoursUtilisateur.getIdGrilleEnCours());
+					if(listLettreEnCour!=null){
+						System.out.println(listLettreEnCour);
+						session.put("listLettre", listLettreEnCour);
+					}else{
+						if(session.containsKey("listLettre")){
+							session.put("listLettre", "");
+						}else
+							session.put("listLettre", "");	
+					}
+				}else{
+					if(session.containsKey("listLettre")){
+						session.put("listLettre", "");
+					}else
+						session.put("listLettre", "");
+				}
+			}else{
+				if(session.containsKey("listLettre")){
+					session.put("listLettre", "");
+				}else
+					session.put("listLettre", "");
+			}
 		} catch (SQLException e) {
 			addActionError(e.getMessage());
 			return ERROR;
@@ -50,5 +93,4 @@ public class PlayAction extends ActionSupport {
 	public Grille getGrille() {
 		return grille;
 	}
-	
 }
